236bddcd1026565e798b80436629af8b7c806848
NavanidhiDJ <navanidhi.cs23@bmsce.ac.in>
2024-12-02 15:32:26 +0530

    Create package cie see main.java

---
+++ package cie see main.java
@@ -0,0 +1,17 @@
+import SEE.Externals;
+
+public class main{
+    public static void main(String args[]){
+	Externals ex=new Externals();
+
+	ex.inputstuddetail();
+
+	ex.inputinternalmarks();
+
+	ex.inputseemarks();
+
+	ex.calculatefinal();
+
+	ex.displayfinal();
+    }
+}
